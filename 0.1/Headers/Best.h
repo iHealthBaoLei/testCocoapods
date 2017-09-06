be89628c5e7354136a919f45c19da7d6cfac9b8b
包磊 <baolei@ihealthlabs.com.cn>
2017-09-06 20:06:49 +0800

    first

---
+++ 0.1/Headers/Best.h
@@ -0,0 +1,15 @@
+//
+//  Best.h
+//  Best
+//
+//  Created by 包磊 on 2017/9/5.
+//  Copyright © 2017年 baolei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Best : NSObject
+
++ (void)printLog:(NSString *)a;
+
+@end
